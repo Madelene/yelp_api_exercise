c3283394c6d84b8eebce12705568b79cd492d24b
Madelene Campos <madelenecampos@gmail.com>
2016-03-28 10:53:49 -0400

    Added validations and association in user model between users and restaurants

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+	has_many :user_restaurants
+	has_many :restaurants, :through => :user_restaurants
+
+	validates :username, presence: true,
+						 uniqueness: true	
+	validates :email, presence: true,
+						 uniqueness: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+
 end
