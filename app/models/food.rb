942b1810e20f7cb45b74057a9324194ffe58019f
Madelene Campos <madelenecampos@gmail.com>
2016-03-28 12:01:01 -0400

    Added associations in food model between users through user_foods and restaurants through restaurant_foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,9 @@
 class Food < ActiveRecord::Base
+	has_many :restaurant_foods
+	has_many :restaurants, :through => :restaurant_foods
+
+	has_many :user_foods
+	has_many :users, :through => :user_foods
+
+	validates :type, presence: true
 end
