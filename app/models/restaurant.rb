d5caa55c687c6a12c3245bb200683583be9831f9
Madelene Campos <madelenecampos@gmail.com>
2016-03-28 11:58:10 -0400

    Added associations in restaurant model between restaurants and foods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,9 @@ class Restaurant < ActiveRecord::Base
 	has_many :user_restaurants
 	has_many :users, :through => :user_restaurants
 
+	has_many :restaurant_foods
+	has_many :foods, :through => :restaurant_foods
+
 	validates :name, presence: true
-	
+
 end
