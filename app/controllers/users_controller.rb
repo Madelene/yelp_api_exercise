5d74a36fef5f3431a309b0bbcceea3e1f3037eb5
Madelene Campos <madelenecampos@gmail.com>
2016-03-28 13:11:41 -0400

    Added create method for users to sign in successfully using a unique username and email, first name and last name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 	protect_from_forgery
-	
+
+
+
+
+	def create
+		new_user_saved = User.new(user_params).save
+
+		if new_user_saved
+			render json: { message: "You have successfully signed up!" }, status: 200
+		else
+			render json: { message: "Please try again" }, status: 200
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :first_name, :last_name)
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ActiveRecord::Base
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 
+
+  user = User.create
 end
