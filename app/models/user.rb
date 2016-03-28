286383e86e1e178cd1f1e401e0dda5580440205d
Madelene Campos <madelenecampos@gmail.com>
2016-03-28 11:53:56 -0400

    Added association in user model between users and foods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 	has_many :user_restaurants
 	has_many :restaurants, :through => :user_restaurants
 
+	has_many :user_foods
+	has_many :foods, :through => :user_foods
+
 	validates :username, presence: true,
 						 uniqueness: true	
 	validates :email, presence: true,
