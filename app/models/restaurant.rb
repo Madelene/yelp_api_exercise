7f11452560661f1d4f7f05f9bf237fdc32085e35
Madelene Campos <madelenecampos@gmail.com>
2016-04-04 10:19:58 -0400

    Fixed spacing in Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,15 @@
 class Restaurant < ActiveRecord::Base
-	has_many :user_restaurants
-	has_many :users, :through => :user_restaurants
+  has_many :user_restaurants
+  has_many :users, :through => :user_restaurants
 
-	has_many :restaurant_foods
-	has_many :foods, :through => :restaurant_foods
+  has_many :restaurant_foods
+  has_many :foods, :through => :restaurant_foods
 
-	validates :name, presence: true
+  validates :name, presence: true
 
+  scope :active, -> { where(is_active: true) }
+
+  def disable
+    update is_active: false
+  end
 end
