5db3eb19a2cf826b264d0fcd5b71720b62857c04
Madelene Campos <madelenecampos@gmail.com>
2016-04-04 10:11:38 -0400

    Fixed spacing in user_foods controller

--- app/controllers/user_foods_controller.rb
+++ app/controllers/user_foods_controller.rb
@@ -1,36 +1,36 @@
 class UserFoodsController < ApplicationController
-	protect_from_forgery
+  protect_from_forgery
 
-	expose(:user_id)				{ params[:user_id] }
-	expose(:food_id)				{ params[:food_id] }
+  expose(:user_id)        { params[:user_id] }
+  expose(:food_id)        { params[:food_id] }
 
-	def favorite
-		favoriting = UserFood.create(user_id: user_id, food_id: food_id)
-		
-		if favoriting
-			name = Food.where(id: food_id).pluck(:food_type)
-			favoriting.save!
-	
-			render json: { food: name }, status: 200
-		else 
-			render json: { message: "You have not favorited any foods yet" }, status: 200
-		end
-	end
+  def favorite
+    favoriting = UserFood.create(user_id: user_id, food_id: food_id)
+    
+    if favoriting
+      name = Food.where(id: food_id).pluck(:food_type)
+      favoriting.save!
+      
+      render json: { food: name }, status: 200
+    else 
+      render json: { message: "You have not favorited any foods yet" }, status: 200
+    end
+  end
 
-	def stop_favoriting
-  		unfavorite_food = UserFood.find_by(food_id: food_id, user_id: user_id)
-  		unfavorite_food.unfavorite
+  def stop_favoriting
+    unfavorite_food = UserFood.find_by(food_id: food_id, user_id: user_id)
+    unfavorite_food.unfavorite
 
-  		render json: { data: "You have now unfavorited this type of food" }, status: 200
-  	end
+    render json: { data: "You have now unfavorited this type of food" }, status: 200
+  end
 
   def favorite_foods
-  		list_of_faves = UserFood.where(user_id: user_id).all 
-  	
-  	if list_of_faves == []
-  		render json: { message: "You still need to favorite some food types!" }, status: 200
-  	else 
-  		render json: { favorite_foods: list_of_faves }, status: 200
-  	end
+    list_of_faves = UserFood.where(user_id: user_id).all 
+    
+    if list_of_faves == []
+      render json: { message: "You still need to favorite some food types!" }, status: 200
+    else 
+      render json: { favorite_foods: list_of_faves }, status: 200
+    end
   end
 end
