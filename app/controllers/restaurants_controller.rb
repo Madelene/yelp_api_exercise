b0cda1a394627b03398fab7ba5ae6348786681ef
Madelene Campos <madelenecampos@gmail.com>
2016-04-04 09:54:01 -0400

    Fixed spacing in Restaurants Controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,16 +2,14 @@ class RestaurantsController < ApplicationController
 	protect_from_forgery
 
 	expose(:id)                   { params[:id] }
-  
   expose(:name)                 { params[:name] }
   expose(:description)          { params[:description] }
   expose(:food_type)            { params[:food_type] }
   expose(:location)          		{ params[:location] }
   expose(:website)            	{ params[:website] }
 
-
-	def index
-		restaurant = Restaurant.all
+  def index
+    restaurant = Restaurant.all
     
     if restaurant
     	render json: { restaurant: restaurant }, status: 200
@@ -30,27 +28,24 @@ class RestaurantsController < ApplicationController
 
   def update
   	found_restaurant = Restaurant.find_by(id: id)
-		restaurant_confirm = found_restaurant.update(restaurant_params)
+    restaurant_confirm = found_restaurant.update(restaurant_params)
 
-      if restaurant_params == nil
+    if restaurant_params == nil
       	render json: { data: 'Please try updating your restaurant again'}, status: 204 #no content
- 			else restaurant_confirm
+      else restaurant_confirm
         render json: {restaurant: restaurant_confirm.as_json }, status: 200
       end
-  end  
+    end  
 
-  def destroy
-  	destroyed_restaurant = Restaurant.find_by(id: id)
-    destroyed_restaurant.disable
-    render json: { data: "Restaurant is now inactive"}, status: 200
-  end
+    def destroy
+     destroyed_restaurant = Restaurant.find_by(id: id)
+     destroyed_restaurant.disable
+     render json: { data: "Restaurant is now inactive"}, status: 200
+   end
 
+   private
 
-
-
-  private
-
-  def restaurant_params
-  	params.require(:restaurant).permit(:name, :description, :food_type, :location, :website)
-  end
-end
+   def restaurant_params
+     params.require(:restaurant).permit(:name, :description, :food_type, :location, :website)
+   end
+ end
