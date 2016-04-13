0f5ca2312c40ada96859342994e3b029b3835051
Madelene Campos <madelenecampos@gmail.com>
2016-04-13 17:56:47 -0400

    Wrote another example, testing the favorite_restaurants method

--- spec/controllers/user_restaurants_controller_spec.rb
+++ spec/controllers/user_restaurants_controller_spec.rb
@@ -11,19 +11,27 @@ RSpec.describe UserRestaurantsController, :type => :controller do
       get :stop_favoriting, restaurant_id: restaurant.id, user_id: user.id
       expect(response).to be_successful
     end
-
+  end
     # it "returns json format" do
-    #   get :stop_favoriting
+    #   restaurant = Restaurant.create!(name: "foobar")
+    #   user = User.create!(username: "foobarssss", email: "as@gmail.com", first_name: "Maddie", last_name: "bar")
+    #   user.restaurants << restaurant
+
+    #   get :stop_favoriting, restaurant_id: restaurant_id, user_id: user_id
     #   expect(response.content_type).to eq "application/json"
     # end
+
+  describe "GET #favorite_restaurants" do
+    it "responds successfully with an HTTP 200 status code" do
+      restaurant = Restaurant.create!(name: "yay")
+      user = User.create!(username: "blahblah", email: "fdsa@gmail.com", first_name: "Whee", last_name: "lkj")
+      user.restaurants << restaurant
+      # UserRestaurant.where(user_id: user_id).all 
+
+      get :favorite_restaurants, user_id: user.id
+      expect(response.status).to eq(200)
+    end
   end
-
-  # describe "GET #favorite_restaurants" do
-  #   it "responds successfully with an HTTP 200 status code" do
-  #     get :favorite_restaurants
-  #     expect(response.status).to eq(200)
-  #   end
-
   #   it "returns json format" do
   #   get :favorite_restaurants
   #   expect(response.content_type).to eq "application/json"
