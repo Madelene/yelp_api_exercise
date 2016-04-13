91c6ee75e4bee9e68245bbe222c8ad47ef8cbb0e
Madelene Campos <madelenecampos@gmail.com>
2016-04-13 12:50:07 -0400

    Wrote 2 examples to test GET methods in controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,21 +8,32 @@ RSpec.describe UsersController, :type => :controller do
       expect(response.status).to eq(200)
     end
 
-    it "returns a list of users" do
+    it "returns json format" do
       get :index
-      expect(response.content_type).to eq "app/json"
+      expect(response.content_type).to eq "application/json"
     end
   end 
 
-  describe "POST #create" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :create
-      expect(response.status).to eq(200)
-    end
+  # describe "POST #create" do
+  #   it "responds successfully with an HTTP 200 status code" do
+  #     post :create
+  #     new_user_saved = User.new(user_params).save
+  #     { "users": [
+  #       {
+  #       "username": "maddiec",
+  #       "email": "maddie@gmail.org",
+  #       "first_name": "Madelene",
+  #       "last_name": "Campski",
+  #       "created_at": "2016-03-28T14:20:43.076Z",
+  #       "updated_at": "2016-03-28T14:20:43.076Z"
+  #       },
+  #       ] }
+  #     expect(response.status).to eq(200)
+  #   end
 
-    it "signs up users" do
-      get :create
-      expect(response).to render_template("new_user_saved")
-    end
-  end
+  #   it "signs up users" do
+  #     post :create
+  #     expect(response).to render_template("new_user_saved")
+  #   end
+  # end
 end
